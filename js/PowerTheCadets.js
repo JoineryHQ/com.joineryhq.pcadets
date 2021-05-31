b7d6f909c040f03b522d62948bd09bfd78463895
Allen Shaw <allen@JoineryHQ.com>
2021-05-31 18:02:43 -0500

    Bug-fix: user-visible strings in JS were  not passed through ts().

--- js/PowerTheCadets.js
+++ js/PowerTheCadets.js
@@ -3,10 +3,10 @@ CRM.$(function($) {
     $('.pcadets-listing-donors-viewmore').click(function(e){
       e.preventDefault();
       if (!$(this).hasClass('is-active')) {
-        $(this).addClass('is-active').find('span').text('Hide Sponsors');
+        $(this).addClass('is-active').find('span').text(ts('Hide Sponsors'));
         $(this).prev().slideDown();
       } else {
-        $(this).removeClass('is-active').find('span').text('View Sponsors');
+        $(this).removeClass('is-active').find('span').text(ts('View Sponsors'));
         $(this).prev().slideUp();
       }
     });
