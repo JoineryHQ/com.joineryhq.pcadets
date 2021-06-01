d2daf446338f95e7e49da683115be0a5df82858e
Allen Shaw <allen@JoineryHQ.com>
2021-05-31 19:57:32 -0500

    Remove redundant $(document).ready() inside of CRM.$().

--- js/PowerTheCadets.js
+++ js/PowerTheCadets.js
@@ -1,14 +1,12 @@
 CRM.$(function($) {
-  $(document).ready(function(){
-    $('.pcadets-listing-donors-viewmore').click(function(e){
-      e.preventDefault();
-      if (!$(this).hasClass('is-active')) {
-        $(this).addClass('is-active').find('span').text(ts('Hide Sponsors'));
-        $(this).prev().slideDown();
-      } else {
-        $(this).removeClass('is-active').find('span').text(ts('View Sponsors'));
-        $(this).prev().slideUp();
-      }
-    });
+  $('.pcadets-listing-donors-viewmore').click(function(e){
+    e.preventDefault();
+    if (!$(this).hasClass('is-active')) {
+      $(this).addClass('is-active').find('span').text(ts('Hide Sponsors'));
+      $(this).prev().slideDown();
+    } else {
+      $(this).removeClass('is-active').find('span').text(ts('View Sponsors'));
+      $(this).prev().slideUp();
+    }
   });
 });
